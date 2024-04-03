081d73a1867d0356089cd50c90b1debdb0959d65
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-04-03 17:00:17 +0100

    order

---
+++ orders/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from carts.models import CartItem
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from .forms import OrderForms
+from .models import Order
+
+# Create your views here.
+
+
+def place_order(request, total=0, quantity=0):
+    current_user = request.user
+
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax
+
+    if request.method == 'POST':
+        form = OrderForms(request.POST)
+        if form.is_valid():
+            data = Order()
+            data.first_name = form.cleaned_data('first_name')
+            data.last_name = form.cleaned_data('last_name')
+            data.phone = form.cleaned_data('phone')
+            data.email = form.cleaned_data('email')
+            data.address_line_1 = form.cleaned_data('address_line_1')
+            data.address_line_2 = form.cleaned_data('address_line_2')
+            data.country = form.cleaned_data('country')
+            data.state = form.cleaned_data('state')
+            data.city = form.cleaned_data('city')
+            data.post_code = form.cleaned_data('post_code')
+            data.order_note = form.cleaned_data('order_note')
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
