dfacaa169db8855b2910afb33af5f1350dd28d28
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-04-06 12:54:05 +0100

    payment completed

--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,32 @@ from django.http import HttpResponse
 from carts.models import CartItem
 from django.shortcuts import render, redirect
 from .forms import OrderForms
-from .models import Order
+from .models import Order, Payment
 import datetime
+import json
 
 # Create your views here.
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=body['orderID'])
+    # store transaction detail inside paymentmodel
+    payment = Payment(
+        user=request.user,
+        payment_id=body['transID'],
+        payment_method=['body.payment_method'],
+        amount_paid=order.order_total,
+        status=body['status'],
+
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
     return render(request, 'orders/payments.html')
 
 
