a6886b29847a8b4d8a039f19ce1b449634215b63
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-04-29 12:48:40 +0100

    urlfiex

--- goldenoud/urls.py
+++ goldenoud/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     path('convert/', include('guest_user.urls')),
 
 ]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
