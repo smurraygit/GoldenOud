da5208311d7076841de88d84ce2f179756a84b7c
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-04-29 12:56:03 +0100

    small change

--- goldenoud/urls.py
+++ goldenoud/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     path('convert/', include('guest_user.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
