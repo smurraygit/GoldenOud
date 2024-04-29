58cb0197e1ae50a97f47f682e4a73a817953260b
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-04-29 12:58:33 +0100

    added one line

--- goldenoud/urls.py
+++ goldenoud/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('convert/', include('guest_user.urls')),
 
 ]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
