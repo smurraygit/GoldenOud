92f71fa48fa3369cad3f85c9c9d81c36e6c8629c
smurraygit <127979150+smurraygit@users.noreply.github.com>
2024-04-29 12:45:31 +0100

    static fixed

--- goldenoud/urls.py
+++ goldenoud/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     path('convert/', include('guest_user.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
